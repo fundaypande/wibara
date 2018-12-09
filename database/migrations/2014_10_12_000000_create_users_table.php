d449fa843deeb405b0d34356ea1b980580a36058
Funday Pande <fundaylogin@gmail.com>
2018-12-09 15:40:14 +0800

    migrate users tabel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::dropIfExists('users');
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('token')->nullable();
+            $table->boolean('status')->default(false);
+            $table->tinyInteger('role')->default('1');
             $table->rememberToken();
             $table->timestamps();
         });
